fdeb2b49a235ffb36ba581724195c7fe3b94fc32
Sai chandra reddy Katukuri <110294689+saiichandrareddy@users.noreply.github.com>
2022-10-11 18:23:04 +0530

    Add files via upload

---
+++ GLlabassignment2_DSA/src/dsa/Traveller/Mainn.java
@@ -0,0 +1,8 @@
+package dsa.Traveller;
+
+public class Mainn {
+	public static void main(String[] args) {
+		DenominationsCalculator main = new DenominationsCalculator();
+		main.minDenominations();
+	}
+}
